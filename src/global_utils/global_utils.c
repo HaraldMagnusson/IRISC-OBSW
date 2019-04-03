59260a4669395f7a4eb39d2a07812b79c9dbe432
Adam Śmiałek <smialekadam@gmail.com>
2019-04-03 23:20:37 +0200

    Move `init_` functions contents to `global_utils.h`

--- src/global_utils/global_utils.c
+++ src/global_utils/global_utils.c
@@ -6,8 +6,33 @@
  * -----------------------------------------------------------------------------
  */
 
+#include <stdio.h>
+#include <string.h>
+
 #include "global_utils.h"
 
 int init_global_utils( void ){
+    return SUCCESS;
+}
+
+int init_submodules(const module_init_t *init_sequence, int module_count) {
+    int ret;
+
+    for(int i=0; i<module_count; ++i){
+        ret = init_sequence[i].init();
+        if( ret == SUCCESS ){
+            fprintf(stderr, "\tSub module \"%s\" initialised successfully.\n",
+                    init_sequence[i].name);
+        } else if( ret == FAILURE ){
+            fprintf(stderr, "\tSub module \"%s\" FAILED TO INITIALISE, return value: %d\n",
+                    init_sequence[i].name, ret);
+            return ret;
+        } else {
+            fprintf(stderr, "\tSub module \"%s\" FAILED TO INITIALISE, return value: %d, %s\n",
+                    init_sequence[i].name, ret, strerror(ret));
+            return ret;
+        }
+    }
+
     return SUCCESS;
 }
