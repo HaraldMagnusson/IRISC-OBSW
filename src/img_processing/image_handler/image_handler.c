08d312a78dc5cdb6bea1afe42632c9504aaef18e
William Eriksson <william@weriksson.space>
2019-08-04 21:22:22 +0200

    Fixed segfault and cleaned code

--- src/img_processing/image_handler/image_handler.c
+++ src/img_processing/image_handler/image_handler.c
@@ -1,7 +1,7 @@
 /* -----------------------------------------------------------------------------
  * Component Name: Image Handler
  * Parent Component: Img Processing
- * Author(s):
+ * Author(s): William Eriksson
  * Purpose: Process and store images along with housekeeping data, as well as
  *          sending it to ground.
  * -----------------------------------------------------------------------------
@@ -12,30 +12,24 @@
 #include <stdlib.h>
 #include <string.h>
 #include <zstd.h>
+#include <errno.h>
+#include <sys/stat.h>
 
-static size_t fread_orDie(void* buffer, size_t sizeToRead, FILE* file)
+static size_t fread_return_size(void* buffer, size_t sizeToRead, FILE* file)
 {
     size_t const readSize = fread(buffer, 1, sizeToRead, file);
-    if (readSize == sizeToRead) return readSize;   /* good */
-    if (feof(file)) return readSize;   /* good, reached end of file */
-    /* error */
-    perror("fread");
-    exit(4);        // --------fixa
-}
-
-static size_t fwrite_orDie(const void* buffer, size_t sizeToWrite, FILE* file)
-{
-    size_t const writtenSize = fwrite(buffer, 1, sizeToWrite, file);
-    if (writtenSize == sizeToWrite) return sizeToWrite;   /* good */
-    /* error */
-    perror("fwrite");
-    exit(5);        // --------fixa
+    if (readSize == sizeToRead) return readSize;
+    if (feof(file)) return readSize;
+    
+    logging(ERROR, "image_handler",
+    "Error while reading file");
+    exit(ERROR);
 }
 
 static void compress_file(const char* file_name_in, const char* file_name_out, int c_level) {
 
-    FILE* const file_in = fopen(file_name_in, "rb");
-    FILE* const file_out = fopen(file_name_out, "wb");
+    FILE* file_in = fopen(file_name_in, "rb");
+    FILE* file_out = fopen(file_name_out, "wb");
 
     size_t const buff_in_size = ZSTD_CStreamInSize();
     void* const buff_in = malloc(buff_in_size);
@@ -43,30 +37,30 @@ static void compress_file(const char* file_name_in, const char* file_name_out, i
     void* const buff_out = malloc(buff_out_size);
 
     ZSTD_CCtx* const cctx = ZSTD_createCCtx();
-    /* check? */
 
     ZSTD_CCtx_setParameter(cctx, ZSTD_c_compressionLevel, c_level);
     ZSTD_CCtx_setParameter(cctx, ZSTD_c_checksumFlag, 1);
 
     size_t const to_read = buff_in_size;
     size_t read;
-    while ((read = fread_orDie(buff_in, to_read, file_in))) {
+    
+    while ((read = fread_return_size(buff_in, to_read, file_in))) {
 
         int const last_chunk = (read < to_read);
+        
         ZSTD_EndDirective const mode = last_chunk ? ZSTD_e_end : ZSTD_e_continue;
-
         ZSTD_inBuffer input = { buff_in, read, 0 };
         int finished;
+        
         do {
             ZSTD_outBuffer output = { buff_out, buff_out_size, 0 };
             size_t const remaining = ZSTD_compressStream2(cctx, &output, &input, mode);
-            /*check_zstd ??*/
-            fwrite_orDie(buff_out, output.pos, file_out);
+
+            fwrite(buff_out, 1, output.pos, file_out);
 
             finished = last_chunk ? (remaining == 0) : (input.pos == input.size);
 
         } while (!finished);
-            /*check input.pos??*/
 
     }
 
@@ -77,42 +71,30 @@ static void compress_file(const char* file_name_in, const char* file_name_out, i
     free(buff_out);
 }
 
+/* compression_stream:
+ * Compresses a file.
+ *
+ * input:
+ * in_filename: filepath of file to be compressed.
+ */
+int compression_stream(const char* in_filename) {
 
-static char* create_outputfilename(const char* filename)
-{
-    size_t const inL = strlen(filename);
-    size_t const outL = inL + 5;
-    char* const outSpace = (char *) malloc(outL);
-    memset(outSpace, 0, outL);
-    strcat(outSpace, filename);
-    strcat(outSpace, ".zst");
-    return (char*)outSpace;
-}
+    size_t const in_length = strlen(in_filename);
+    size_t const out_length = in_length + 5;
+    char* const out_filename = (char *) malloc(out_length);
+    memset(out_filename, 0, out_length);
+    strcat(out_filename, in_filename);
+    strcat(out_filename, ".zst");
 
-int compression_stream(const char* argv) {
-
-    const char* const in_filename = argv;
-    char* const out_filename = create_outputfilename(in_filename);
     compress_file(in_filename, out_filename, 1);
-
     free(out_filename);
 
-    return 0;
+    return SUCCESS;
 
-}
-
-int test() {
-
-
-    char intput_file[] = "/irisc-obsw/src/img_processing/image_handler/test.FIT";
-        compression_stream(intput_file);
-
-    return 2;
 }
 
 int init_image_handler(void) {
 
-    test();
-
     return SUCCESS;
+
 }
