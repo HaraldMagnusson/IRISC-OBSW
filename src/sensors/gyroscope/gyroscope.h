fa8e80fed3971a76761cafd6bfea281aa1bb680a
Harald Magnusson <harald.magnusson@hotmail.se>
2019-08-28 16:34:42 +0200

    Gyro component to store gyro data

--- src/sensors/gyroscope/gyroscope.h
+++ src/sensors/gyroscope/gyroscope.h
@@ -1,7 +1,7 @@
 /* -----------------------------------------------------------------------------
  * Component Name: Gyroscope
  * Parent Component: Sensors
- * Author(s):
+ * Author(s): Harald Magnusson
  * Purpose: Keep track of the angular motion of the telescope.
  *
  * -----------------------------------------------------------------------------
@@ -9,4 +9,22 @@
 
 #pragma once
 
-int init_gyroscope( void );
+typedef struct{
+    double x, y, z;
+
+    /* This flag shows if the available data is the latest (value: 0)
+     * or if an error occured in the gyro module while updating (value: 1).
+     * If an error has occured, the data in (lat, lon, alt) is the
+     * latest valid data.
+     */
+    char out_of_date;
+} gyro_t;
+
+int init_gyroscope(void);
+
+void get_gyro(gyro_t* gyro);
+
+void set_gyro(gyro_t* gyro);
+
+/* set the out of date flag on the gyro data */
+void gyro_out_of_date(void);
