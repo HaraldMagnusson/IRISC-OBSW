ca1e365c73b20966686ce5a39e2e5cd0e4513a0b
Harald Magnusson <harald.magnusson@hotmail.se>
2019-09-11 12:06:05 +0200

    Created function for debug call to tetra

--- src/sensors/sensor_poller/star_tracker_poller/star_tracker_poller.c
+++ src/sensors/sensor_poller/star_tracker_poller/star_tracker_poller.c
@@ -16,6 +16,7 @@
 #define TETRAPATH "Tetra/tetra.py"
 
 static void irisc_tetra(float st_return[]);
+static int call_tetra(float st_return[]);
 static void* st_poller_thread(void* arg);
 
 static pthread_t st_poller_id;
@@ -33,42 +34,54 @@ static void* st_poller_thread(void* arg){
 
     float st_return[4];
 
-    #ifdef ST_DEBUG
-        struct timespec samp_0, samp, diff;
-    #endif
-
     clock_gettime(CLOCK_MONOTONIC, &wake);
     wake.tv_sec++;
 
     while(1){
-        #ifdef ST_DEBUG
-            clock_gettime(CLOCK_MONOTONIC, &samp_0);
-            irisc_tetra(st_return);
-            clock_gettime(CLOCK_MONOTONIC, &samp);
-
-            diff.tv_sec = samp.tv_sec - samp_0.tv_sec;
-            diff.tv_nsec = samp.tv_nsec - samp_0.tv_nsec;
-            if(diff.tv_nsec < 0){
-                diff.tv_sec--;
-                diff.tv_nsec += 1000000000;
-            }
-
-            logging(DEBUG, "Star Tracker", "Star tracker sample time: %ld.%09ld s",
-                    diff.tv_sec, diff.tv_nsec);
-            logging(DEBUG, "Star Tracker", "Output: %f, %f, %f, %f",
-                    st_return[0], st_return[1], st_return[2], st_return[3]);
-        #else
-            irisc_tetra(st_return);
-        #endif
-
-        if(st_return[3] == 0){
-            logging(WARN, "Star Tracker", "FoV = 0");
+        if(call_tetra(st_return)){
+            /* set out of date */
             continue;
         }
+
+
+
     }
     return NULL;
 }
 
+
+static int call_tetra(float st_return[]){
+
+    #ifdef ST_DEBUG
+        struct timespec samp_0, samp, diff;
+    #endif
+
+    #ifdef ST_DEBUG
+        clock_gettime(CLOCK_MONOTONIC, &samp_0);
+        irisc_tetra(st_return);
+        clock_gettime(CLOCK_MONOTONIC, &samp);
+
+        diff.tv_sec = samp.tv_sec - samp_0.tv_sec;
+        diff.tv_nsec = samp.tv_nsec - samp_0.tv_nsec;
+        if(diff.tv_nsec < 0){
+            diff.tv_sec--;
+            diff.tv_nsec += 1000000000;
+        }
+
+        logging(DEBUG, "Star Tracker", "Star tracker sample time: %ld.%09ld s",
+                diff.tv_sec, diff.tv_nsec);
+        logging(DEBUG, "Star Tracker", "Output: %f, %f, %f, %f",
+                st_return[0], st_return[1], st_return[2], st_return[3]);
+    #else
+        irisc_tetra(st_return);
+    #endif
+
+    if(st_return[3] == 0){
+        logging(WARN, "Star Tracker", "FoV = 0, lost in space failed");
+        return FAILURE;
+    }
+    return SUCCESS;
+}
 
 
 /*
