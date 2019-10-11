b69a0a14e33afd8151a9b7c81678fb62b1aa7601
Harald Magnusson <harald.magnusson@hotmail.se>
2019-10-11 10:26:23 +0200

    Added control system loop in stabilization

--- src/control_sys/stabilization/stabilization.c
+++ src/control_sys/stabilization/stabilization.c
@@ -1,31 +1,28 @@
 /* -----------------------------------------------------------------------------
- * Component Name: Controller
+ * Component Name: Stabilization
  * Parent Component: Control System
- * Author(s): Adam Śmiałek
- * Purpose: Stabilise the telescope on the current target.
+ * Author(s): Harald Magnusson
+ * Purpose: Combining the kalman filter, pid controller, and motor control
+ *          to create the entire control system.
  *
- *
- * Functions for use as telecommands:
- *  - change_pid_values - changes current pid values until next mode change
- *  - change_mode_pid_values - permanently changes pid values for specified mode
- *
- * Functions for external call:
- *  - change_stabilization_mode - use to change to stabilization mode at the
- *                                start of exposure and back to tracking at the
- *                                end of it
- *  - pid_reset - to be used alongside change_stabilization_mode if deemed
- *                necessary
  * -----------------------------------------------------------------------------
  */
 
+#include <pthread.h>
+
+#include "current_target.h"
 #include "global_utils.h"
+#include "mode.h"
+#include "pid.h"
+#include "kalman_filter.h"
+#include "gimbal.h"
+
+static void* control_sys_thread(void* args);
 
 int init_stabilization(void* args){
-    return SUCCESS;
+    return create_thread("control_system", control_sys_thread, 30);
 }
 
-#if 0
-// structure for control system thread with KF + PID
 pthread_mutex_t mutex_cond_cont_sys = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond_cont_sys = PTHREAD_COND_INITIALIZER;
 
@@ -34,7 +31,7 @@ static void* control_sys_thread(void* args){
 
     struct timespec wake_time;
 
-    double motor_out[2];
+    //double motor_out[2];
     telescope_att_t cur_pos;
 
     while(1){
@@ -46,9 +43,9 @@ static void* control_sys_thread(void* args){
         while(get_mode() != RESET){
 
             kf_update(&cur_pos);
-            pid_update(&cur_pos, motor_out);
+            //pid_update(&cur_pos, motor_out);
 
-            motor_output(motor_out);
+            //motor_output(motor_out);
 
             wake_time.tv_nsec += CONTROL_SYS_WAIT;
             if(wake_time.tv_nsec >= 1000000000){
@@ -61,4 +58,3 @@ static void* control_sys_thread(void* args){
 
     return NULL;
 }
-#endif
