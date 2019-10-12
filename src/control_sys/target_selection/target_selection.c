ec651896440a0ec166a140bb05067093d8566325
Harald Magnusson <harald.magnusson@hotmail.se>
2019-10-12 21:43:15 +0200

    Restructured selection and tracking code

--- src/control_sys/target_selection/target_selection.c
+++ src/control_sys/target_selection/target_selection.c
@@ -20,6 +20,8 @@
 #include "camera.h"
 
 static void* sel_track_thread_func(void* arg);
+static void selection(int* tar_index);
+static int tracking(int tar_index, char exposing_flag);
 
 static double d_mod(double val, int mod);
 static void angle_calc(double dec, double ha,
@@ -27,34 +29,18 @@ static void angle_calc(double dec, double ha,
 static void fetch_time(double* ut_hours, double* j2000);
 
 static int exp_time = 30, sensor_gain = 100;
-static pthread_t sel_track_thread_id;
 
 static double az_threshold = 0.01, alt_threshold = 0.01;
 
 
 int init_target_selection(void* args){
 
-    pthread_create(&sel_track_thread_id, NULL, sel_track_thread_func, NULL);
-
-    return SUCCESS;
+    return create_thread("select_track", sel_track_thread_func, 30);
 }
 
 static void* sel_track_thread_func(void* arg){
 
-    int ret, tar_index;
-
-    char exposing_flag;
-
     struct timespec wake;
-
-    gps_t gps;
-    encoder_t enc;
-    telescope_att_t telescope_att;
-
-    double j2000, ut_hours, lst, ha, alt, az, max_prio = 0, gon_az;
-
-    target_prio_t target_prio[19];
-    target_t target_list_aa[19], target_err;
 
     clock_gettime(CLOCK_MONOTONIC, &wake);
     wake.tv_sec++;
@@ -63,118 +49,15 @@ static void* sel_track_thread_func(void* arg){
     while(1){
 
         /* reset camera axis to center */
+        int tar_index;
+        selection(&tar_index);
 
-        /* fetch data: gps, time, gondola attitude(kalman filter + encoder) */
-        /* time */
-        fetch_time(&ut_hours, &j2000);
+        char exposing_flag = 0;
 
-        /* gps */
-        get_gps(&gps);
-
-        /* encoder */
-        get_encoder(&enc);
-
-        /* kalman */
-        get_telescope_att(&telescope_att);
-
-        /* select target */
-        lst = 100.46 + 0.985647 * j2000 + gps.lon + 15 * ut_hours;
-        lst = d_mod(lst, 360);
-
-        gon_az = telescope_att.az - enc.az;
-
-        for(int ii=0; ii<19; ++ii){
-            /* calculate alt, az, ha for all targets*/
-            target_list_aa[ii].ha = lst - 15 * target_list_rd[ii].ra;
-            angle_calc(target_list_rd[ii].dec, target_list_aa[ii].ha,
-                    gps.lat, &alt, &az);
-            target_list_aa[ii].alt = alt;
-            target_list_aa[ii].az = az;
-
-            /* position parameter */
-            target_prio[ii].pos_param = gon_az - target_list_aa[ii].az;
-            target_prio[ii].pos_param = fabs(target_prio[ii].pos_param) < OP_FOV/3 ?
-                    OP_FOV/3 - target_prio[ii].pos_param : 0;
-
-            /* exposure parameter */
-            target_prio[ii].exp_param = target_prio[ii].exp_num > 10 ?
-                    0.1 : exp_prio_list[target_prio[ii].exp_num];
-
-            /* total priority */
-            target_prio[ii].tot_prio = target_list_rd[ii].mag *
-                    target_prio[ii].pos_param * target_prio[ii].exp_param *
-                    target_list_rd[ii].type_prio;
-        }
-
-        /* finding maximum priority target */
-        for(int ii=0; ii<19; ++ii){
-            if( target_prio[ii].tot_prio > max_prio ){
-                max_prio = target_prio[ii].tot_prio;
-                tar_index = ii;
-            }
-        }
-
-        #ifdef SELECTION_DEBUG
-            logging(DEBUG, "Selection", "Current target: %s", target_list_rd[tar_index].name);
-        #endif
-
-        /* selection done */
-        exposing_flag = 0;
         /* tracking */
         while(1){
 
-            /* fetch data */
-            fetch_time(&ut_hours, &j2000);
-            get_gps(&gps);
-            get_encoder(&enc);
-            get_telescope_att(&telescope_att);
-
-            /* calculate tracking angles */
-            lst = 100.46 + 0.985647 * j2000 + gps.lon + 15 * ut_hours;
-            lst = d_mod(lst, 360);
-
-            ha = lst - 15 * target_list_rd[tar_index].ra;
-            angle_calc(target_list_rd[tar_index].dec, ha, gps.lat, &alt, &az);
-
-            set_tracking_angles(az, alt);
-
-            #ifdef TRACKING_DEBUG
-//                logging(DEBUG, "Tracking", "Tracking angles: az: %+9.4lf, alt: %+9.4lf",
-//                        az, alt);
-            #endif
-
-            /* abort exposure if target is moving out of operational FoV */
-            if(fabs(enc.az) > OP_FOV * 0.45){
-                if(exposing_flag){
-                    abort_exp_nir();
-                    logging(WARN, "Tracking",
-                            "Aborted exposure due to telescope leaving operational FoV.");
-                }
-                break;
-            }
-
-            /* camera control */
-            target_err.az = az - telescope_att.az;
-            target_err.alt = alt - telescope_att.alt;
-
-            if(     !exposing_flag                  &&
-                    target_err.az < az_threshold    &&
-                    target_err.alt < alt_threshold) {
-
-                /* start exp */
-                expose_nir(exp_time, sensor_gain);
-                exposing_flag = 1;
-                /* set exposing flag */
-            }
-
-            if(exposing_flag){
-                /* save image */
-                ret = save_img_nir();
-                if(ret != EXP_NOT_READY){
-                    break;
-                }
-            }
-            /* end of camera control */
+            tracking(tar_index, exposing_flag);
 
             /* wait for one sample time */
             wake.tv_nsec += TRACKING_UPDATE_TIME;
@@ -187,6 +70,134 @@ static void* sel_track_thread_func(void* arg){
     }
 
     return NULL;
+}
+
+static void selection(int* tar_index){
+    /* fetch data: gps, time, gondola attitude(kalman filter + encoder) */
+    /* time */
+    double ut_hours, j2000;
+    fetch_time(&ut_hours, &j2000);
+
+    /* gps */
+    gps_t gps;
+    get_gps(&gps);
+
+    /* encoder */
+    encoder_t enc;
+    get_encoder(&enc);
+
+    /* kalman */
+    telescope_att_t telescope_att;
+    get_telescope_att(&telescope_att);
+
+    /* select target */
+    double lst = 100.46 + 0.985647 * j2000 + gps.lon + 15 * ut_hours;
+    lst = d_mod(lst, 360);
+
+    double gon_az = telescope_att.az - enc.az;
+
+    target_prio_t target_prio[19];
+    target_t target_list_aa[19];
+
+    for(int ii=0; ii<19; ++ii){
+        /* calculate alt, az, ha for all targets*/
+        target_list_aa[ii].ha = lst - 15 * target_list_rd[ii].ra;
+
+        double az = 0, alt = 0;
+        angle_calc(target_list_rd[ii].dec, target_list_aa[ii].ha,
+                gps.lat, &alt, &az);
+        target_list_aa[ii].alt = alt;
+        target_list_aa[ii].az = az;
+
+        /* position parameter */
+        target_prio[ii].pos_param = gon_az - target_list_aa[ii].az;
+        target_prio[ii].pos_param = fabs(target_prio[ii].pos_param) < OP_FOV/3 ?
+                OP_FOV/3 - target_prio[ii].pos_param : 0;
+
+        /* exposure parameter */
+        target_prio[ii].exp_param = target_prio[ii].exp_num > 10 ?
+                0.1 : exp_prio_list[target_prio[ii].exp_num];
+
+        /* total priority */
+        target_prio[ii].tot_prio = target_list_rd[ii].mag *
+                target_prio[ii].pos_param * target_prio[ii].exp_param *
+                target_list_rd[ii].type_prio;
+    }
+
+    /* finding maximum priority target */
+    double max_prio = 0;
+    for(int ii=0; ii<19; ++ii){
+        if( target_prio[ii].tot_prio > max_prio ){
+            max_prio = target_prio[ii].tot_prio;
+            *tar_index = ii;
+        }
+    }
+
+    #ifdef SELECTION_DEBUG
+        logging(DEBUG, "Selection", "Current target: %s", target_list_rd[tar_index].name);
+    #endif
+}
+
+static int tracking(int tar_index, char exposing_flag){
+    /* fetch data */
+    double ut_hours, j2000;
+    fetch_time(&ut_hours, &j2000);
+
+    gps_t gps;
+    get_gps(&gps);
+
+    encoder_t enc;
+    get_encoder(&enc);
+
+    telescope_att_t telescope_att;
+    get_telescope_att(&telescope_att);
+
+    /* calculate tracking angles */
+    double lst = 100.46 + 0.985647 * j2000 + gps.lon + 15 * ut_hours;
+    lst = d_mod(lst, 360);
+
+    double ha = lst - 15 * target_list_rd[tar_index].ra;
+    double az, alt;
+    angle_calc(target_list_rd[tar_index].dec, ha, gps.lat, &alt, &az);
+
+    set_tracking_angles(az, alt);
+
+    #ifdef TRACKING_DEBUG
+        logging(DEBUG, "Tracking", "Tracking angles: az: %+9.4lf, alt: %+9.4lf",
+                az, alt);
+    #endif
+
+    /* abort exposure if target is moving out of operational FoV */
+    if(fabs(enc.az) > OP_FOV * 0.45){
+        if(exposing_flag){
+            abort_exp_nir();
+            logging(WARN, "Tracking",
+                    "Aborted exposure due to telescope leaving operational FoV.");
+        }
+        return FAILURE;
+    }
+
+    /* camera control */
+    target_t target_err;
+    target_err.az = az - telescope_att.az;
+    target_err.alt = alt - telescope_att.alt;
+
+    if(     !exposing_flag                  &&
+            target_err.az < az_threshold    &&
+            target_err.alt < alt_threshold) {
+
+        expose_nir(exp_time, sensor_gain);
+        exposing_flag = 1;
+    }
+
+    if(exposing_flag){
+        /* save image */
+        if(save_img_nir() != EXP_NOT_READY){
+            return FAILURE;
+        }
+    }
+
+    return SUCCESS;
 }
 
 /* Modulo opperation on doubles */
