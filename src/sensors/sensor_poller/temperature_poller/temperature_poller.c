a0a758fb2eacc969f60be583502a5f96e671037f
Harald Magnusson <harald.magnusson@hotmail.se>
2019-10-15 22:07:18 +0200

    Added i2c bus

--- src/sensors/sensor_poller/temperature_poller/temperature_poller.c
+++ src/sensors/sensor_poller/temperature_poller/temperature_poller.c
@@ -13,7 +13,16 @@
 static void* temp_poller_thread(void* args);
 static void active_m(void);
 
+static int fd_i2c;
+
 int init_temperature_poller(void* args){
+
+    fd_i2c = open("/dev/i2c-1", O_RDWR);
+    if(fd_i2c == -1){
+        logging(ERROR, "Temp_poller", "Failed to open i2c-1 device: %m");
+        return errno;
+    }
+
     return create_thread("temp_poller", temp_poller_thread, 20);
 }
 
